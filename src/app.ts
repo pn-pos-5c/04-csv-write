4be6edbf08604c470d30cbabaad697d87d4fc79e
Philipp <philipp.niemetz@gmail.com>
2021-09-25 17:03:48 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,39 @@
 const consoleArguments: string[] = process.argv.splice(2);
-const consoleArgument: string = consoleArguments[0];
-let random: number;
+let revenue: number;
+let count: number = 1000;
 
 if (consoleArguments.length > 1) {
     console.error('Too many arguments');
     process.exit(-1);
-} else if (consoleArgument === undefined || consoleArgument === '-l') {
-    random = Math.floor(Math.random() * 11);
-} else if (consoleArgument === '-h') {
-    random = Math.floor(Math.random() * 1001);
+} else if (consoleArguments.length === 1 && consoleArguments[0] === undefined) {
+
+} else if (consoleArguments[0] === undefined || consoleArguments.includes('-l')) {
+    revenue = Math.floor(Math.random() * 11);
+} else if (consoleArguments.includes('-h')) {
+    revenue = Math.floor(Math.random() * 1001);
 } else {
     console.error(`Unknown argument "${consoleArguments[0]}"`);
     process.exit(-1);
 }
 
-console.log(random);
+if (consoleArguments.includes('-n')) {
+
+}
+
+if (consoleArguments[0] === undefined) {
+    revenue = Math.floor(Math.random() * 11);
+} else {
+
+}
+
+for (let i = 0; i < consoleArguments.length; i++) {
+    if (consoleArguments[i] === '-h') {
+        revenue = Math.floor(Math.random() * 1001);
+    } else if (consoleArguments[i] === '-n') {
+
+    } else if (consoleArguments[i] === '-l') {
+        revenue = Math.floor(Math.random() * 11);
+    }
+}
+
+console.log(revenue);
